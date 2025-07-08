602298fd4ee6b8917b42ff66724ee715216fb71b
SaoulaHocine <59206202+SaoulaHocine@users.noreply.github.com>
2025-07-08 02:05:02 +0200

    Update phone number to +213554261617

---
+++ src/config/websiteData.ts
@@ -0,0 +1,8 @@
+@@ .. @@
+  // Contact Information
+  contact: {
+-    phone: "(970) 555-0123",
+-    phoneLink: "tel:+19705550123",
++    phone: "+213 554 261 617",
++    phoneLink: "tel:+213554261617",
+    email: "mrsaoulahocine@gmail.com",
